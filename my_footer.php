c314f88030a259f4f4762ff9a71f78691f49ceed
Timothy B. Booher <Tim@Timothy-B-Boohers-MacBook-Pro.local>
2010-05-06 05:58:40 -0400

    first add

---
+++ my_footer.php
@@ -0,0 +1,4 @@
+<?php // add your own content here ?>
+<a href="<?php bloginfo('rss2_url'); ?>"><img alt="<?php _e('rss',TEMPLATE_DOMAIN); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/images/rss_blog.gif"/></a>
+<a href="<?php bloginfo('comments_rss2_url'); ?>"><img alt="<?php _e('Comments rss',TEMPLATE_DOMAIN); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/images/rss_comments.gif"/></a>
+<a href="http://validator.w3.org/check?uri=referer"><img alt="<?php _e('valid xhtml 1.1',TEMPLATE_DOMAIN); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/images/valid_xhtml11_80x15_2.png"/></a>
---
+++ my_sidebar_left.php
@@ -0,0 +1,3 @@
+<?php // add your own content here ?>
+
+foo
