1a62cacaec12389365d4914a1f0fcfc2ae8e3ef0
Tim Booher <ads@theboohers.org>
2010-05-09 22:22:07 -0400

    formatting changes

--- my_footer.php
+++ my_footer.php
@@ -3,18 +3,17 @@
 
 <?php dynamic_sidebar(1) ?>
 <div class="widget">
-<ul>
-    <li><a href="http://www.cafepress.com/cfadaptation">Store</a></li>
-    <li><a href="/contact_us">Contact</a></li>
-    <li><a href="/">Home</a></li>
-<li><a href="<?php bloginfo('rss2_url'); ?>"><img alt="<?php _e('rss',TEMPLATE_DOMAIN); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/images/rss_blog.gif"/></a></li>
-<li><a href="<?php bloginfo('comments_rss2_url'); ?>"><img alt="<?php _e('Comments rss',TEMPLATE_DOMAIN); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/images/rss_comments.gif"/></a></li>
-<li><a href="http://validator.w3.org/check?uri=referer"><img alt="<?php _e('valid xhtml 1.1',TEMPLATE_DOMAIN); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/images/valid_xhtml11_80x15_2.png"/></a></li>
-			    <h2><?php _e('Search',TEMPLATE_DOMAIN); ?></h2>
-			    <?php include (TEMPLATEPATH . '/searchform.php'); ?>
-			
 
-
-</ul>
+<div>
+<h4><a href="http://www.cafepress.com/cfadaptation">Store</a></h4> |
+<h4> <a href="/contact_us">Contact Us</a></h4> |
+<h4> <a href="/">Home</a></h4>
+</div>
+<div>
+<a href="<?php bloginfo('rss2_url'); ?>"><img alt="<?php _e('rss',TEMPLATE_DOMAIN); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/images/rss_blog.gif"/></a>  <a href="<?php bloginfo('comments_rss2_url'); ?>"><img alt="<?php _e('Comments rss',TEMPLATE_DOMAIN); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/images/rss_comments.gif"/></a>
+</div>
+<?php include (TEMPLATEPATH . '/searchform.php'); ?>
+<p>&copy; 2010 CrossFit Adaptation</p>
+<p>2620 S. Shirlington Rd. Arlington, VA 22206</p>
 </div>
 <div style="clear:both;"></div>
--- my_sidebar.php
+++ my_sidebar.php
@@ -1,29 +1,56 @@
+<link href="/wp-content/themes/freshy2/custom_cooking.css" rel="stylesheet" type="text/css" />
 <h2>Schedule:</h2>
 
-<h4>Monday-Friday</h4>
+<h4>Workouts</h4>
 
-<div><strong>am</strong>: 5:30 | 6:30 | 9:30</div>
-<div><strong>Skill Work Hour</strong>: 11 am</div>
-<div><strong>pm</strong>: 12:00 | 1:00 | 5:30 | 6:30</div>
-
-<h4>Saturday</h4>
-<div>9:00 | 10:00 | 11:00 am</div>
-<h4>Sunday</h4>
-<div>10:00 | 11:00 am </div>
+<table>
+<tr>
+<td class="col1">
+   M-F am</td>
+<td>
+   5:30
+</td>
+<td>
+   6:30
+</td>
+<td>
+   9:30
+</td>
+<td>11:00*</td>
+</tr>
+<tr>
+  <td class="col1">M-F pm</td>
+  <td>12:00</td>
+  <td>1:00</td>
+  <td>5:30</td>
+  <td>6:30</td>
+</tr>
+<tr>
+  <td class="col1">Sat</td>
+  <td>9am</td>
+  <td>10am</td>
+  <td>11am</td>
+  <td>&nbsp;</td>
+</tr>
+<tr>
+  <td class="col1">Sun</td>
+  <td>&nbsp;</td>
+  <td>10am</td>
+  <td>11am</td>
+  <td>&nbsp;</td>
+</tr>
+</table>
+<p>* Skills session</p>
 
 <h4>Yoga</h4>
 Tuesday/Thursday: 8 pm, Sunday: 7 pm
 
 <h2><a href="/location/">Directions</a></h2>
 
-!! address here !!
-
 <h2><a href="/coaches/" target="_blank">Team</a></h2>
-
-<i>need to add</i>
-
-<p>crossfit journal</p>
-<p>crossfit games</p>
-<p>crossfit ffr</p>
-
-add contact
+<hr />
+<div>
+<a target="_blank" href="http://journal.crossfit.com/2002/10/what-is-fitness-by-greg-glassm.tpl">
+<img width="144" height="61" border="0" alt="CrossFit Journal Sample Article...right here" src="/wp-content/themes/freshy2/images/CrossFitJournal.gif">
+</a>
+</div>
