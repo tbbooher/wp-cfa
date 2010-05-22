90fbc790c090273487e2697750bac5d54fed48ae
Tim Booher <ads@theboohers.org>
2010-05-22 17:03:59 -0400

    new

--- my_footer.php
+++ my_footer.php
@@ -12,7 +12,7 @@
 <div>
 <div>
 <a target="_blank" href="http://journal.crossfit.com/2002/10/what-is-fitness-by-greg-glassm.tpl">
-<img width="144" height="61" border="0" alt="CrossFit Journal Sample Article...right here" src="/wp-content/themes/wp-cfa/images/CrossFitJournal.gif">
+<img width="144" height="61" alt="CrossFit Journal Sample Article...right here" src="/wp-content/themes/wp-cfa/images/CrossFitJournal.gif">
 </a>
 </div>
 <a href="<?php bloginfo('rss2_url'); ?>"><img alt="<?php _e('rss',TEMPLATE_DOMAIN); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/images/rss_blog.gif"/></a>  <a href="<?php bloginfo('comments_rss2_url'); ?>"><img alt="<?php _e('Comments rss',TEMPLATE_DOMAIN); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/images/rss_comments.gif"/></a>
