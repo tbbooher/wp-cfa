53c54baf7c314eeb4883ad92d1073c99597edb6f
Tim Booher <ads@theboohers.org>
2010-05-22 17:11:26 -0400

    new

--- searchform.php
+++ searchform.php
@@ -1,1 +1,5 @@
-<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/"><input size="10" type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" /><input class="btn" type="submit" id="searchsubmit" value="<?php _e('Search',TEMPLATE_DOMAIN); ?>" /></form>
+<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+<fieldset>
+<input size="10" type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" /><input class="btn" type="submit" id="searchsubmit" value="<?php _e('Search',TEMPLATE_DOMAIN); ?>" />
+</fieldset>
+</form>
